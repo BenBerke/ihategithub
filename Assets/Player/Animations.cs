e732ad1d315ade02e9fffa7d4569876ab887b96e
BenBerke <77770248+BenBerke@users.noreply.github.com>
2021-02-05 21:31:37 +0300

    Add files via upload

---
+++ Assets/Player/Animations.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animations : MonoBehaviour
+{
+
+    public Animator animator;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+            animator.SetBool("Yürüme", true);
+            
+        }
+
+        else 
+        {
+
+            animator.SetBool("Yürüme", false);
+
+        }
+
+    }
+}
